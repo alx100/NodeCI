6de1d98b6091ce9ede3302dd28a7777dbf09e03b
alx100 <stotsky.alex@gmail.com>
2018-04-19 15:14:36 +0300

    Initial commit

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID: '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb://alx100:alx100@ds149138.mlab.com:49138/blog_dev',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
